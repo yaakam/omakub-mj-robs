8a515afe00dd734026145d1bac3b1c86d773fdd2
AkitaOnRails <fabioakita@gmail.com>
2024-10-28 12:05:03 -0300

    adding support for arch linux, not sure if this will finish, but should allow testing

--- install/check-version.sh
+++ install/check-version.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 if [ ! -f /etc/os-release ]; then
-    echo "$(tput setaf 1)Error: Unable to determine OS. /etc/os-release file not found."
-    echo "Installation stopped."
-    exit 1
+  echo "$(tput setaf 1)Error: Unable to determine OS. /etc/os-release file not found."
+  echo "Installation stopped."
+  exit 1
 fi
 
 . /etc/os-release
 
 # Check if running on Manjaro Linux
-if [ "$ID" != "manjaro" ]; then
-    echo "$(tput setaf 1)Error: OS requirement not met"
-    echo "You are currently running: $ID"
-    echo "OS required: Manjaro GNOME 24 or higher"
-    echo "Installation stopped."
-    exit 1
+if [ "$ID" != "manjaro" ] && [ "$ID" != "arch" ]; then
+  echo "$(tput setaf 1)Error: OS requirement not met"
+  echo "You are currently running: $ID"
+  echo "OS required: Manjaro GNOME 24+ or Arch Linux"
+  echo "Installation stopped."
+  exit 1
 fi
