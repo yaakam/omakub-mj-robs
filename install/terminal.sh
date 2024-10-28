c5d156bf72e6d1d36140a5bdb304979921c324e7
AkitaOnRails <fabioakita@gmail.com>
2024-10-28 13:16:46 -0300

    when running only terminal installers, there is no finish message, so adding one

--- install/terminal.sh
+++ install/terminal.sh
@@ -4,3 +4,5 @@ sudo pacman -S curl git unzip --noconfirm
 
 # Run terminal installers
 for installer in ~/.local/share/omakub/install/terminal/*.sh; do source $installer; done
+
+echo "Finished installing all Terminal-based apps and configurations. Log out and log back in."
