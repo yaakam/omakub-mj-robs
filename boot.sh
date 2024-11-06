13644aff6a584abd63a6430c401d47768b45c82e
Djaiton <drobertes.dev@gmail.com>
2024-11-06 20:45:28 -0300

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -16,7 +16,7 @@ yay -Syyuu --noconfirm
 
 echo "Cloning Omakub..."
 rm -rf ~/.local/share/omakub
-git clone https://github.com/akitaonrails/omakub.git ~/.local/share/omakub >/dev/null
+git clone https://github.com/yaakam/omakub-mj-robs.git ~/.local/share/omakub >/dev/null
 if [[ $OMAKUB_REF != "master" ]]; then
   cd ~/.local/share/omakub
   git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
