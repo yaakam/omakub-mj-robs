4d89a46fa320155ddb4b5164897ab3a6faddc3dd
Djaiton <drobertes.dev@gmail.com>
2024-11-06 19:26:22 -0300

    Update terminal.sh

--- install/terminal.sh
+++ install/terminal.sh
@@ -1,6 +1,6 @@
 # Needed for all installers
 sudo pacman -Syyuu --noconfirm
-sudo pacman -S curl git unzip --noconfirm
+sudo pacman -S curl git unzip base-devel --noconfirm
 
 # Run terminal installers
 for installer in ~/.local/share/omakub/install/terminal/*.sh; do source $installer; done
